66e1e99ea4973c87916563fc2da7c3762a5e52d0
Allan Anderson <47725827+sarge-al@users.noreply.github.com>
2020-06-04 13:50:23 -0400

    Update Project2.js

--- Project2.js
+++ Project2.js
@@ -33,13 +33,12 @@ function myFilter(arr,call){
 	return newArr;
 }
 
-//Without using the native “Array.prototype.some” method of JavaScript, compose a function titled “mySome” that will take in an array of elements and executes a callback function on each of those elements.
-function 
+//Without using the native “Array.prototype.some” method of JavaScript, compose a function titled “mySome” that will take in an array of elements and executes a callback function on each of those elements. 
 function mySome(arr,call){
 	for(let i=0; i<arr.length; i++){
 		if(call(arr[i])){
-			return true;
-		}
+		return true;
+	}
 	return false;
 }
 
